ba6878789c4055389079ae9c2be24e84a8819ae7
gema_handika <gemahandika88@gmail.com>
2025-01-10 15:51:30 +0700

    update

--- app/Controller/Asset_controller.php
+++ app/Controller/Asset_controller.php
@@ -2,29 +2,13 @@
 require_once "../Config/koneksi.php";
 require_once "../Asset/sweetalert/dist/func_sweetAlert.php";
 
-if (isset($_POST['create_sca'])) {
-    $kurir_id = trim(mysqli_real_escape_string($koneksi, $_POST['kurir_id']));
-    $password_sca = trim(mysqli_real_escape_string($koneksi, $_POST['password_sca']));
-    $fullname_sca = trim(mysqli_real_escape_string($koneksi, $_POST['fullname_sca']));
-    $nik_sca = trim(mysqli_real_escape_string($koneksi, $_POST['nik_sca']));
-    $phone_sca = trim(mysqli_real_escape_string($koneksi, $_POST['phone_sca']));
-    $zona_sca = trim(mysqli_real_escape_string($koneksi, $_POST['zona_sca']));
-    $cabang = trim(mysqli_real_escape_string($koneksi, $_POST['cabang']));
-    $status_sca = trim(mysqli_real_escape_string($koneksi, $_POST['status_sca']));
-    $jobtask_sca = trim(mysqli_real_escape_string($koneksi, $_POST['jobtask_sca']));
-
-    // Validasi Username agar tidak ganda
-    $check_query = "SELECT * FROM tb_sca WHERE kurir_id = '$kurir_id'";
-    $check_result = $koneksi->query($check_query);
-    if ($check_result->num_rows > 0) {
-        showSweetAlert('warning', 'Oops...', $pesan, '#3085d6', '../../public/views/sca/index.php');
-    } else {
-
-        // Masukan data ke tabel anggota
-        mysqli_query($koneksi, "INSERT INTO tb_sca ( kurir_id, password_sca , fullname_sca, nik_sca, phone_sca, zona_sca, cabang_sca,status_sca,jobtask_sca) 
-    VALUES( '$kurir_id', '$password_sca', '$fullname_sca', '$nik_sca', '$phone_sca','$zona_sca','$cabang','$status_sca','$jobtask_sca')");
-        showSweetAlert('success', 'Sukses', $pesan_ok, '#3085d6', '../../public/views/sca/index.php');
-    }
+if (isset($_POST['create_asset'])) {
+    $pc_name = trim(mysqli_real_escape_string($koneksi, $_POST['pc_name']));
+    $fleet = trim(mysqli_real_escape_string($koneksi, $_POST['fleet']));
+    // Masukan data ke tabel anggota
+    mysqli_query($koneksi, "INSERT INTO tb_asset ( pc_name, fleet) 
+    VALUES( '$pc_name', '$fleet')");
+    showSweetAlert('success', 'Sukses', $pesan_ok, '#3085d6', '../../public/views/tb_asset/index.php');
 } else if (isset($_POST['edit_asset'])) {
     $id = $_POST['id_asset'];
     $divisi_asset = trim(mysqli_real_escape_string($koneksi, $_POST['divisi_asset']));
