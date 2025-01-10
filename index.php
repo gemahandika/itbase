8fed9ae2ae3461e7d96aaa298ee272523ce75e12
gema_handika <gemahandika88@gmail.com>
2025-01-10 18:14:51 +0700

    update

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset='utf-8'>
   <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-  <title>AdminLTE v4</title>
+  <title>ITBase JNE MEDAN</title>
   <meta name='viewport' content='width=device-width, initial-scale=1'>
 </head>
 
