a5c65855c9a2bccfeddc854c344ee3ebeb859a77
gema_handika <gemahandika88@gmail.com>
2025-02-05 16:10:07 +0700

    dashboard

--- public/views/dashboard/button_cabang.php
+++ public/views/dashboard/button_cabang.php
@@ -1,11 +1,23 @@
+<!-- Tambahkan jQuery jika belum ada -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
 <div class="row">
-    <div class="col-lg-12 col-12">
-        <div class="card card-info card-outline mb-2">
-            <div class="card-body">
-                <div class="btn-group mb-2 flex-wrap d-flex" role="group" aria-label="Basic outlined example">
-                    <a href="home.php" type="button" class="btn btn-outline-primary mb-2 mr-2">HOME</a>
-                    <a href="medan.php" type="button" class="btn btn-outline-primary mb-2 mr-2">MEDAN</a>
-                    <button type="button" class="btn btn-outline-primary mb-2 mr-2">PAKAM</button>
+    <div class="col-12">
+        <div class="card mb-2">
+            <button type="button" class="btn  toggle-card" title="Collapse">
+                <div class="card-header">
+                    <h3 class="card-title">Pilih Cabang</h3>
+                    <div class="card-tools">
+                        <i class="bi bi-chevron-down"></i>
+                        <i class="bi bi-chevron-up d-none"></i>
+                    </div>
+                </div>
+            </button>
+            <div class="card-body" style="display: none;">
+                <div class="btn-group mb-2 flex-wrap d-flex" role="group">
+                    <a href="home.php" class="btn btn-outline-primary mb-2 mr-2">HOME</a>
+                    <a href="medan.php" class="btn btn-outline-primary mb-2 mr-2">MEDAN</a>
+                    <a href="pakam.php" class="btn btn-outline-primary mb-2 mr-2">PAKAM</a>
                     <button type="button" class="btn btn-outline-primary mb-2 mr-2">PAYAGELI</button>
                     <button type="button" class="btn btn-outline-primary mb-2 mr-2">SERGEI</button>
                     <button type="button" class="btn btn-outline-primary mb-2 mr-2">BINJAI</button>
@@ -28,3 +40,17 @@
         </div>
     </div>
 </div>
+
+<script>
+    $(document).ready(function() {
+        $(".toggle-card").click(function() {
+            var cardBody = $(this).closest(".card").find(".card-body");
+            var downIcon = $(this).find(".bi-chevron-down");
+            var upIcon = $(this).find(".bi-chevron-up");
+
+            cardBody.slideToggle(300); // Efek buka/tutup dengan durasi 300ms
+            downIcon.toggleClass("d-none");
+            upIcon.toggleClass("d-none");
+        });
+    });
+</script>
