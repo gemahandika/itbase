ca23de064febb814a77cbe9df1153fa1b3a4c1ee
gema_handika <gemahandika88@gmail.com>
2025-03-05 11:38:19 +0700

    userhybrid

--- app/Config/koneksi.php
+++ app/Config/koneksi.php
@@ -1,12 +1,12 @@
 <?php
 // koneksi local
-$koneksi = mysqli_connect('localhost', 'root', '', 'db_itbase');
-if (mysqli_connect_errno()) {
-    echo mysqli_connect_error();
-}
-
-// koneksi public
-// $koneksi = mysqli_connect('localhost', 'jnee6778_mesit', 'Jnemes2017', 'jnee6778_itbase');
+// $koneksi = mysqli_connect('localhost', 'root', '', 'db_itbase');
 // if (mysqli_connect_errno()) {
 //     echo mysqli_connect_error();
 // }
+
+// koneksi public
+$koneksi = mysqli_connect('localhost', 'jnee6778_mesit', 'Jnemes2017', 'jnee6778_itbase');
+if (mysqli_connect_errno()) {
+    echo mysqli_connect_error();
+}
