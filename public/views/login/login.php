93e932e6808cdfd6f49429ab91746775338c9c2c
gema_handika <gemahandika88@gmail.com>
2025-05-19 14:44:46 +0700

    update

--- public/views/login/login.php
+++ public/views/login/login.php
@@ -91,9 +91,7 @@ if (isset($_POST['login'])) {
   <meta name="title" content="AdminLTE 4 | Login Page" />
   <meta name="author" content="ColorlibHQ" />
   <link rel="shortcut icon" href="../../../app/Asset/img/jne/JNE.png">
-  <meta
-    name="description"
-    content="AdminLTE is a Free Bootstrap 5 Admin Dashboard, 30 example pages using Vanilla JS." />
+
   <meta
     name="keywords"
     content="bootstrap 5, bootstrap, bootstrap 5 admin dashboard, bootstrap 5 dashboard, bootstrap 5 charts, bootstrap 5 calendar, bootstrap 5 datepicker, bootstrap 5 tables, bootstrap 5 datatable, vanilla js datatable, colorlibhq, colorlibhq dashboard, colorlibhq admin dashboard" />
